b35c52401e266c0aa819ce748514eab65360f482
morellid <root@HPLab.(none)>
2012-11-06 16:45:36 +0100

    script that shows how to match an expr

--- CodeRunner/Script.fsx
+++ CodeRunner/Script.fsx
@@ -1,7 +1,6 @@
 ﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
-#load "Library1.fs"
 open EnergyPatterns
 open Microsoft.FSharp.Quotations
 
@@ -9,43 +8,12 @@ open Microsoft.FSharp.Quotations
 //let kr = new KernelRunner()
 
 
-type KernelRunner(expr: Expr) = 
-    // To implement
-    let isValid(e:Expr) =
-      e.CustomAttributes
-      true
-
-
-type Kernel() =
-  inherit System.Attribute()
-
-// come cavolo si mettono i custom attributes in una Expr??
-let expr : Expr<int> = <@ 1 + 1 @>
-
-
-[<Kernel>]
-
 [<ReflectedDefinition>]
-let f a = a } 2  1
+let foo a = a * a
+let e = <@ foo @>
 
-let expr = <@ f @>
+match e with
+  | ExprShape.ShapeVar v -> printf "ShapeVar"
+  | ExprShape.ShapeLambda(v,e) -> printf "ShapeLambda"
+  | ExprShape.ShapeCombination(o,e) -> printf "ShapeCombination"
 
-a.
-a.CustomAttributes
-
-[<ReflectedDefinition>]
-let expr : Expr<int> = <@ 1 + 1 @>
-expr.CustomAttributes
-
-
-// usando reflection standard (OK)
-type Demo =
- [<Kernel>]
-  static member AddOne x = x + 1
-
-let ty = typeof<Demo>
-let mi = ty.GetMethod("AddOne")
-mi.CustomAttributes
-typeof<Kernel>
-let at = mi.GetCustomAttributes(typeof<Kernel>, false)
-mi
