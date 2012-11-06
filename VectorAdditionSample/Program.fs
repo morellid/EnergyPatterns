3078e459170819305cfe511bb95963e026ec4cdd
Gabriele Cocco <cocco@di.unipi.it>
2012-11-06 18:38:12 +0100

    Added vector types provider and kernel sample (VectorAdditionSample)

---
+++ VectorAdditionSample/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open FSCL
+
+[<EntryPoint>]
+let main argv = 
+    let t = new FSCL.OpenCLVector4D()
+    t.s0
+    t.xw <- [| 0.; 1. |]
+    t.s1230 <- [| 1.;2.;3.;4. |]
+    let g = t.even
+    let g2 = t.odd
+    printfn "%A" argv
+    
+    0 // return an integer exit code
