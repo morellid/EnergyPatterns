2c42bb79cf59e52a1afdc05521d5f2b20943b607
morellid <morellid@di.unipi.it>
2012-11-06 17:02:07 +0100

    more expressive visit of the expr

--- CodeRunner/Script.fsx
+++ CodeRunner/Script.fsx
@@ -7,13 +7,34 @@ open Microsoft.FSharp.Quotations
 // Define your library scripting code here
 //let kr = new KernelRunner()
 
+let rec visit vars expr =
+  let printval (v:Var) = 
+    printf "ShapeVar:%s=%s " (v.Type.Name) (v.ToString())
+  match expr with
+  | ExprShape.ShapeVar v -> 
+     printval v
+  | ExprShape.ShapeLambda(v,e) -> 
+     printf "ShapeLambda(" 
+     printval v
+     printf ","
+     visit Map.empty e
+     printf ")"
+  | ExprShape.ShapeCombination(o,e) -> 
+     printf "ShapeCombination("
+     List.iter (fun ex -> visit Map.empty ex) e
+     printf ")"
+  | _ -> printf "boh... "
+
+  
 
 [<ReflectedDefinition>]
-let foo a = a * a
+let foo a = 
+  let b = 1
+  a + b
+
 let e = <@ foo @>
 
-match e with
-  | ExprShape.ShapeVar v -> printf "ShapeVar"
-  | ExprShape.ShapeLambda(v,e) -> printf "ShapeLambda"
-  | ExprShape.ShapeCombination(o,e) -> printf "ShapeCombination"
+visit Map.empty e
+
+
 
