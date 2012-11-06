1748c27c04696ef6f8bb5db5c8032dadb34059e9
morellid <root@HPLab.(none)>
2012-11-06 15:36:55 +0100

    adding test script

---
+++ Ammeters/RemoteAmmeter/RemoteAmmeter/test.fsx
@@ -0,0 +1,14 @@
+﻿#I @"C:\Users\root\Desktop\EnergyPatterns\Ammeters\RemoteAmmeter\RemoteAmmeter\bin\Debug"
+#r @"RemoteAmmeter.dll"
+open EnergyPatterns.RemoteAmmeter
+
+
+let s = new Server()
+s.Start()
+
+let c = new Client("131.114.88.115")
+c.start()
+
+c.stop()
+
+s.Stop()
