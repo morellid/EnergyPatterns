1e4b44451cff9aab6e1ff0d7b58ea933e614ede1
morellid <morellid@di.unipi.it>
2012-11-07 18:09:12 +0100

    matching operators like + and <

--- CodeRunner/Script.fsx
+++ CodeRunner/Script.fsx
@@ -3,6 +3,7 @@
 
 open EnergyPatterns
 open Microsoft.FSharp.Quotations
+open Quotations.DerivedPatterns
 
 // Define your library scripting code here
 //let kr = new KernelRunner()
@@ -11,10 +12,15 @@ open Microsoft.FSharp.Quotations
 // http://fsharpsamples.codeplex.com/SourceControl/changeset/view/7af524066395#Samples%2fQuotationsVisualizer%2fQuotationsVisualizer%2fProgram.fs
 // http://fortysix-and-two.blogspot.it/2009/06/traversing-and-transforming-f.html
 
+let additionexpr =  <@ (+) @> 
+let lessthanexpr =  <@ (<) @> 
+
 let rec visit vars expr =
   let printval (v:Var) = 
     printf "ShapeVar:%s=%s " (v.Type.Name) (v.ToString())
   match expr with
+  | SpecificCall <@ (+) @> (e1, types, elist) -> printfn "Plus! types=%A exprs=%A" types elist 
+  | SpecificCall <@ (<) @> (e1, types, elist) -> printfn "Minus!  types=%A exprs=%A" types elist 
   | Patterns.Let(v, e1, e2) -> 
      printf "Let (%s:%s, e1=" (v.Type.Name) (v.ToString())
      visit Map.empty e1
@@ -23,11 +29,6 @@ let rec visit vars expr =
      printf ")"
   | Patterns.Call(body, DerivedPatterns.MethodWithReflectedDefinition meth, args) ->
        printf "Call(" 
-       match body with
-       | Some b ->
-          printf "body="
-          visit Map.empty b
-       | _ -> ()
        printf "meth="
        visit Map.empty meth
        printf ", args=["
@@ -58,28 +59,46 @@ let rec visit vars expr =
      visit Map.empty e
      printf ")"
   | ExprShape.ShapeCombination(o,e) ->  // qui va espanso meglio
-     printf "ShapeCombination( o=%s, list=[" (o.ToString()) 
-     match o with
-     | Quotations.DerivedPatterns.Bool(e,b) -> printf "shape"
+     printf "ShapeCombination( list=[" 
      List.iter (fun ex -> visit Map.empty ex) e
      printf "]) "
   | _ -> printf "boh... "
 
-[<ReflectedDefinition>]
+[<ReflectedDefinitionAttribute>]
 let foo a = 
   let b = 1 in
     a + b
 
-[<ReflectedDefinition>]
+[<ReflectedDefinitionAttribute>]
 let foo a = 
     if a < 0 then
       a - 1
     else
       a + 1
 
+[<ReflectedDefinitionAttribute>]
+let foo a = a < 0
+  
 let e = <@ foo @>
 
 visit Map.empty e
 e
+
+
+
+<@ (+) @>
+
+
+
+
+( |SpecificCall|_| ) : Expr -> (Expr -> (Type list * Expr list) option)
+
+let recognizePlus quotation =
+    match quotation with
+    | SpecificCall <@ (+) @> (types,exprs) -> printfn "Plus! types=%A  exprs=%A" types exprs
+    | _ -> printfn "something else"
+
+Quotations.DerivedPatterns.SpecificCall
+
 
 
