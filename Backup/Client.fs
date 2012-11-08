efcfd28163f27e84de0cf4d298418a8e3da598a9
Gabriele Cocco <cocco@di.unipi.it>
2012-11-08 16:26:09 +0100

    Impliementing InstructionEnergy metric (continue). Now considering operations.

---
+++ Backup/Client.fs
@@ -0,0 +1,24 @@
+﻿namespace EnergyPatterns.RemoteAmmeter
+
+open System
+open System.Net
+open System.Text
+open Energon.Measuring
+open System.Collections.Generic
+
+type Client(ip:string) =
+    let wc = new WebClient()
+    let address = System.String.Format(@"http://{0}/Temporary_Listen_Addresses", ip)
+    member x.start() =
+        let addr = System.String.Format(@"{0}/{1}", address, "start")
+        try
+            wc.DownloadString(addr) 
+        with 
+            | _ -> "KO"
+    member x.stop(par:seq<string>) =
+        let addr = System.String.Format(@"{0}/{1}", address, "stop")
+        try
+            wc.DownloadString(addr) 
+        with 
+            | _ -> "KO"
+
---
+++ InstructionEnergyMetric/InstructionCountEstimator.fs
@@ -0,0 +1,90 @@
+﻿namespace InstructionEnergyMetric
+
+open Microsoft.FSharp.Quotations
+
+module InstructionCountEstimator =
+    let ReduceList (f: Expr -> float, l: Expr list) =
+        l |> List.map f |> List.reduce(fun a b -> a + b)
+
+    let rec Estimate (expr: Expr) =
+        match expr with
+        | Patterns.Let (var, v, b) ->
+            (float) (Estimate(b))
+        | Patterns.AddressOf (e) ->
+            Estimate(e)
+        | Patterns.AddressSet (e, v) ->
+            Estimate(e) + Estimate(v)
+        | Patterns.Application (a, b) ->
+            Estimate(a) + Estimate(b)
+        | Patterns.Coerce (e, t) ->
+            Estimate(e)
+        | Patterns.DefaultValue (t) ->
+            0.0
+        | Patterns.FieldGet (e, i) ->
+            if e.IsSome then
+                Estimate(e.Value)
+            else
+                0.0
+        | Patterns.FieldSet (e, i, v) ->
+            if e.IsSome then
+                Estimate(e.Value) + Estimate(v)
+            else
+                Estimate(v)
+        | Patterns.IfThenElse (c, ib, eb) ->
+            Estimate(c) + (0.5 * Estimate(ib)) + (0.5 * Estimate(eb))
+        | Patterns.Lambda (vr, e) ->
+            Estimate(e)
+        | Patterns.NewArray (t, l) ->
+            ReduceList(Estimate, l)
+        | Patterns.NewDelegate (t, vr, e) ->
+            Estimate(e)
+        | Patterns.NewObject (t, l) ->
+           ReduceList(Estimate, l)
+        | Patterns.NewRecord (t, l) ->
+            ReduceList(Estimate, l)
+        | Patterns.NewTuple (l) ->
+            ReduceList(Estimate, l)
+        | Patterns.NewUnionCase (i, l) ->
+            ReduceList(Estimate, l)
+        | Patterns.PropertyGet (e, i, l) ->
+            if e.IsSome then
+                Estimate(e.Value) + ReduceList(Estimate, l)
+            else
+                ReduceList(Estimate, l)
+        | Patterns.PropertySet (e, i, l, v) ->
+            if e.IsSome then
+                Estimate(e.Value) + ReduceList(Estimate, l) + Estimate(v)
+            else
+                ReduceList(Estimate, l) + Estimate(v)
+        | Patterns.Sequential (e1, e2) ->
+            ReduceList(Estimate, [ e1; e2 ])
+        | Patterns.TryFinally (e1, e2) ->
+            ReduceList(Estimate, [ e1; e2 ])
+        | Patterns.TupleGet (e, c) ->
+            Estimate(e)
+        | Patterns.TypeTest (e, t) ->
+            Estimate(e)
+        | Patterns.UnionCaseTest (e, i) ->
+            Estimate(e)
+        | Patterns.VarSet(v, e) ->
+            Estimate(e)
+        | DerivedPatterns.SpecificCall <@ (+) @> (e, t, a) 
+        | DerivedPatterns.SpecificCall <@ (-) @> (e, t, a) 
+        | DerivedPatterns.SpecificCall <@ (*) @> (e, t, a) 
+        | DerivedPatterns.SpecificCall <@ (/) @> (e, t, a) 
+        | DerivedPatterns.SpecificCall <@ (%) @> (e, t, a) 
+        | DerivedPatterns.SpecificCall <@ (&&) @> (e, t, a) 
+        | DerivedPatterns.SpecificCall <@ (||) @> (e, t, a) 
+        | DerivedPatterns.SpecificCall <@ (&&&) @> (e, t, a) 
+        | DerivedPatterns.SpecificCall <@ (|||) @> (e, t, a) 
+        | DerivedPatterns.SpecificCall <@ (<<<) @> (e, t, a) 
+        | DerivedPatterns.SpecificCall <@ (>>>) @> (e, t, a) 
+        | DerivedPatterns.SpecificCall <@ (^^^) @> (e, t, a) 
+        | DerivedPatterns.SpecificCall <@ (~-) @> (e, t, a) 
+        | DerivedPatterns.SpecificCall <@ (~+) @> (e, t, a) ->
+            if e.IsSome then
+                Estimate(e.Value) + ReduceList(Estimate, a) + 1.0
+            else 
+                ReduceList(Estimate, a) + 1.0
+        | _ -> 0.0
+
